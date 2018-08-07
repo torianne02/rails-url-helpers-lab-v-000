a0b920d1a510b4f72daacce46551908c59fae439
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-07 22:15:53 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,11 @@ class StudentsController < ApplicationController
   end
 
   def activate 
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save 
 
+    redirect_to student_path(@student)
   end
 
   private
